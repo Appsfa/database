8e0c90d68e6614c2f456638ebcdfb0a5c86cd218
Appsfa <A01273925@itesm.mx>
2018-04-15 21:06:17 -0500

    1

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 	<header style="height: auto">
   <nav>
     <div class="nav-wrapper black flow-text" style="padding-left: 3%; padding-right: 3%;">
-      <a class="brand-logo">Automatas</a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <a href="http://www.apps-fa.com/proyects/database/" class="brand-logo">BASE DE DATOS</a>
+      <a  data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="http://www.apps-fa.com/proyects/automatas/">Inicio</a></li>
+        <li><a href="http://www.apps-fa.com/proyects/database/">Inicio</a></li>
         <li><a href="http://www.apps-fa.com/proyects/automatas/automatas.php">Automatas</a></li>
         <li><a href="collapsible.html">About Us</a></li>
       </ul>
@@ -12,7 +12,7 @@
   </nav>
 
   <ul class="sidenav" id="mobile-demo">
-    <li><a href="http://www.apps-fa.com/proyects/automatas/">Inicio</a></li>
+    <li><a href="http://www.apps-fa.com/proyects/database/">Inicio</a></li>
         <li><a href="http://www.apps-fa.com/proyects/automatas/automatas.php">Automatas</a></li>
         <li><a href="collapsible.html">About Us</a></li>
   </ul>
