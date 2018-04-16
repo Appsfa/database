ca00faefbe47c2e5084d923f44f19734765e09b2
Appsfa <A01273925@itesm.mx>
2018-04-15 23:02:19 -0500

    1

--- index.php
+++ index.php
@@ -9,10 +9,40 @@
 <?php
 include "header.php";
 ?>
-<body>
-HOLA
+<body  style="background-image: url(img/bg-main.png)">
+	<main class="row">
+		<div class="col s12 m1 l2"></div>
+		<div class="col s12 m10 l8">
+			<br><br>
+			<a href="http://www.apps-fa.com/proyects/database/alumnos.php" class="center col s12 m6" style="margin-top: 4%">
+				<div class="col s12 light-blue lighten-2 white-text">
+					<i class="material-icons"><h1>face</h1></i>
+				</div>
+				<div class="col s12 light-blue lighten-2 white-text">
+					<h4>ALUMNOS</h4>
+				</div>
+				<div class="col s12 light-blue lighten-2">
+					<br>
+					<br>
+				</div>
+			</a>
+			<a href="http://www.apps-fa.com/proyects/database/materias.php" class="center col s12 m6" style="margin-top: 4%">
+				<div class="col s12 pink white-text">
+					<i class="material-icons"><h1>assignment</h1></i>
+				</div>
+				<div class="col s12 pink white-text">
+					<h4>MATERIAS</h4>
+				</div>
+				<div class="col s12 pink">
+					<br>
+					<br>
+				</div>
+			</a>
+		</div>
+		<div class="col s12 m1 l2"></div>
+	</main>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript" src="http://www.apps-fa.com/proyects/automatas/js/materialize.js"></script>
+<script type="text/javascript" src="http://www.apps-fa.com/proyects/database/js/materialize.js"></script>
 <script>
 function hora()
 {
@@ -22,7 +52,6 @@ var minutos = fecha.getMinutes();
 var segundos = fecha.getSeconds();
 
 var horaActual = horas + ":" + minutos + ":" + segundos;
-console.log(horaActual);
 $("#time").html(horaActual);
 setTimeout(hora, 1000);
 }
@@ -37,7 +66,6 @@ $(document).ready(function(){
 
 	$('.sidenav').sidenav();
 });
-$("main").height($( window ).height());
 
 </script>
 </body>
