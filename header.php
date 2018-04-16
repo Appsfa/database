4f1d939fd0c886979724caa662cbd207e51d3177
Appsfa <A01273925@itesm.mx>
2018-04-15 21:09:19 -0500

    1

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
 	<header style="height: auto">
   <nav>
-    <div class="nav-wrapper black flow-text" style="padding-left: 3%; padding-right: 3%;">
+    <div class="nav-wrapper light-blue flow-text" style="padding-left: 3%; padding-right: 3%;">
       <a href="http://www.apps-fa.com/proyects/database/" class="brand-logo">BASE DE DATOS</a>
       <a  data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="http://www.apps-fa.com/proyects/database/">Inicio</a></li>
-        <li><a href="http://www.apps-fa.com/proyects/automatas/automatas.php">Automatas</a></li>
         <li><a href="collapsible.html">About Us</a></li>
       </ul>
     </div>
@@ -13,7 +12,6 @@
 
   <ul class="sidenav" id="mobile-demo">
     <li><a href="http://www.apps-fa.com/proyects/database/">Inicio</a></li>
-        <li><a href="http://www.apps-fa.com/proyects/automatas/automatas.php">Automatas</a></li>
         <li><a href="collapsible.html">About Us</a></li>
   </ul>
 	</header>
