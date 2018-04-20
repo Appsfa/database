2682c87fe04aed23f85bbd3c4914bc5ad04cc0c9
Appsfa <A01273925@itesm.mx>
2018-04-20 14:46:47 -0500

    index modificado
    modificado

--- index.php
+++ index.php
@@ -9,8 +9,29 @@
 <?php
 include "header.php";
 ?>
-<body>
-HOLA
+<body style="background-image: url(http://apps-fa.com/proyects/database/img/bg-main.png)">
+	<main class="row">
+		<div class="col s12 m1 l2"></div>
+		<div class="col s12 m10 l8">
+			<br><br>
+			<div class="col s12 m6">
+				<a class="col s12 light-blue white-text center">
+					<h1><i class="material-icons large">face</i></h1>
+					<h4>ALUMNOS</h4>
+				</a>
+			</div>
+			<div class="col s12 hide-on-med-and-up">
+				<br><br>
+			</div>
+			<div class="col s12 m6">
+				<a class="col s12 pink white-text center">
+					<h1><i class="material-icons large">book</i></h1>
+					<h4>MATERIAS</h4>
+				</a>
+			</div>
+		</div>
+		<div class="col s12 m1 l2"></div>
+	</main>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript" src="http://www.apps-fa.com/proyects/automatas/js/materialize.js"></script>
 <script>
